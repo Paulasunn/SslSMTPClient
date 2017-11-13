18b4d57e7d1b2e48d83501c183daede5dcdfd5ee
breakermind <hello@breakermind.com>
2017-11-13 22:45:56 +0100

    C++ ssl smtp client
    C++ ssl smtp client

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 #define FAIL    -1
 
 // apt-get install libssl-dev openssl
-// g++ -o output main.cpp -lssl -lcrypto -lresolv -std=c++11
+// g++ -o start main.cpp sslsmtpex.cpp sslsmtpex.h -lssl -lcrypto -lresolv -std=c++11 -std=c++14
 
 // Include client
 #include "sslsmtpex.h"
@@ -22,19 +22,29 @@ using namespace std;
 // main - create SSL context and connect
 int main(int count, char *strings[])
 { 
-    cout << "Start smtp\r\n";    
+    cout << "C++ ssl smtp send email with STARTTLS\r\n";    
+
     // Attachments
     vector<string> files;
-    files.push_back("file9.jpg");
-    files.push_back("filek.pdf");
+    // files.push_back("file9.jpg");
+    // files.push_back("filek.pdf");
 
-    sslsmtpEx smtp = sslsmtpEx("",1);
-    bool mailsend1 = smtp.SendAll("<ho@breakermind.com>", "ho@qflash.pl", "<ho@breakermind.com>", "Hello =2C_czy_um=F3wisz_si=EA_ze_mn=B1=3F?=", "Wiadomosć tekstowa dla usera", "<h1>Hello from html msg.</h1>", files);
-    cout << " Send email " << mailsend1 << endl;
+    sslsmtpEx sm;
+    sm.sslsmtpExSet("localhost", 25); 
     
-    // SslSMTP smtp = SslSMTP("",25);
-    // cout << smtp.Date(1) << endl;
-    // smtp.Send("aspmx.l.google.com", "", "", "");
-    // add files path
+    // get MX records for recipient
+    vector<string> mx = sm.getMX("nanomoow@gmail.com",0,0);
+
+    while(true){
+        for (int i = 0; i < mx.size(); i++){
+            // Set hostname from mx dns
+            sm.sslsmtpExSet(mx.at(i), 25);
+            cout << "Mx host: " << mx.at(i) << endl;    
+            // send email            
+            cout << sm.Send("your@email.xx", "nanomoow@gmail.com", "your@email.xx", "Smtp client test", "<h1>Smtp test</h1>", "<h1>Smtp test</h1>", files) << endl;
+        }
+        sleep(10);
+    }
+
 return 0;    
 }
